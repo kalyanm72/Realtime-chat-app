43a6743af8e0557604e8097d7d017b14e7bde801
kalyanm72 <62393289+kalyanm72@users.noreply.github.com>
2020-06-02 12:03:35 +0530

    Update MyFirebase.js

--- video-chat/src/Config/MyFirebase.js
+++ video-chat/src/Config/MyFirebase.js
@@ -1,14 +1,8 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyBlLVDGn4bcSy3jXuCVYVUk231bRmWElVw",
-authDomain: "miniproject-26e19.firebaseapp.com",
-databaseURL: "https://miniproject-26e19.firebaseio.com",
-projectId: "miniproject-26e19",
-storageBucket: "miniproject-26e19.appspot.com",
-messagingSenderId: "44564231383",
-appId: "1:44564231383:web:10028f23860e26d6b92c92",
-measurementId: "G-YSS77Y1MRC"
+  
+
 }
 firebase.initializeApp(config)
 firebase.firestore().settings({
